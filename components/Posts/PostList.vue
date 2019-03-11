c18cd79a99ae2802418056074842b8d059e3e7f8
zwright8 <zacwright27@gmail.com>
2019-03-11 06:40:26 -0400

    Add files via upload

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class = "post-list">
+      <PostPreview
+        id="1"
+        :is-admin="isAdmin"
+        thumbnail ="https://previews.123rf.com/images/elen1/elen11711/elen1171100102/89714894-circuit-board-electronic-computer-hardware-technology-motherboard-digital-chip-tech-science-backgrou.jpg" 
+        title="First Post"
+        previewText="My first post"/>
+      <PostPreview
+        id="2"
+        :is-admin="isAdmin"
+        thumbnail ="https://previews.123rf.com/images/elen1/elen11711/elen1171100102/89714894-circuit-board-electronic-computer-hardware-technology-motherboard-digital-chip-tech-science-backgrou.jpg" 
+        title="Second Post"
+        previewText="My second post"/>
+      <PostPreview
+        id="3"
+        :is-admin="isAdmin"
+        thumbnail ="https://previews.123rf.com/images/elen1/elen11711/elen1171100102/89714894-circuit-board-electronic-computer-hardware-technology-motherboard-digital-chip-tech-science-backgrou.jpg" 
+        title="Third Post"
+        previewText="My third post"/>
+    </section>
+</template>
+
+<script>
+import PostPreview from '~/components/Posts/PostPreview'
+export default {
+    components:{
+        PostPreview
+    },
+    props:{
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '~/components/Admin/AdminPostForm'
+
+export default {
+    layout: 'admin',
+    components:{
+        AdminPostForm
+    },
+    data(){
+        return {
+            loadedPost:{
+                author: 'Zach Wright',
+                title: 'My Post',
+                content: 'This is the content of the post',
+                thumbnailLink: 'https://previews.123rf.com/images/elen1/elen11711/elen1171100102/89714894-circuit-board-electronic-computer-hardware-technology-motherboard-digital-chip-tech-science-backgrou.jpg'
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
